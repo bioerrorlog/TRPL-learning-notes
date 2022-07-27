c25face91f410e0e19c209e9c64f563cbaaef1ac
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-07-27 09:37:48 +0900

    add sample: invalid array element access

--- 3_projects/array/src/main.rs
+++ 3_projects/array/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // Index access
     let a = [1, 2, 3, 4, 5];
@@ -5,4 +7,26 @@ fn main() {
     let first = a[0];
     let second = a[1];
     println!("{}, {}", first, second);
+
+    let a = [1, 2, 3, 4, 5];
+
+    // Invalid array element access
+    println!("Please enter an array index.");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
