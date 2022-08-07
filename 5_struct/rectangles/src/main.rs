dde8a933e7457d3a0c45de5352940458be90d818
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-07 17:16:36 +0900

    refactor: using method

--- 5_struct/rectangles/src/main.rs
+++ 5_struct/rectangles/src/main.rs
@@ -3,6 +3,13 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }    
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -12,10 +19,6 @@ fn main() {
 
     println!(
         "The area of the ractanngle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
