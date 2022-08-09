ab255efb8e22b31e31a15dceb4bb346dccfe1e08
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-09 23:28:55 +0900

    bring Result

---
+++ 7_crates/use_as/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(unused)]
+fn main() {
+    use std::fmt;
+    use std::io;
+
+    fn function1() -> fmt::Result {
+        // --snip--
+        Ok(())
+    }
+
+    fn function2() -> io::Result<()> {
+        // --snip--
+        Ok(())
+    }
+}
