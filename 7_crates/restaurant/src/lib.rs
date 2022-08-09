9e845a0c8199d49d6d0eac8c70caf0ae24781734
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-09 22:54:54 +0900

    add super example

--- 7_crates/restaurant/src/lib.rs
+++ 7_crates/restaurant/src/lib.rs
@@ -11,6 +11,15 @@ mod front_of_house {
         fn serve_order() {}
 
         fn take_payment() {}
+
+        mod back_of_house {
+            fn fix_incorrect_order() {
+                cook_order();
+                super::serve_order();
+            }
+
+            fn cook_order() {}
+        }
     }
 }
 
