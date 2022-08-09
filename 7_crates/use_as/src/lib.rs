a685879ccac6f37162157a4214bfac956678eeb6
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-09 23:29:58 +0900

    add as example

--- 7_crates/use_as/src/lib.rs
+++ 7_crates/use_as/src/lib.rs
@@ -1,14 +1,14 @@
 #![allow(unused)]
 fn main() {
-    use std::fmt;
-    use std::io;
+    use std::fmt::Result;
+    use std::io::Result as IoResult;
 
-    fn function1() -> fmt::Result {
+    fn function1() -> Result {
         // --snip--
         Ok(())
     }
 
-    fn function2() -> io::Result<()> {
+    fn function2() -> IoResult<()> {
         // --snip--
         Ok(())
     }
