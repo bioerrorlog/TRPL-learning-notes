8fefc2f22ef2342e54baf0e385f194cab6b17063
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-11 12:57:16 +0900

    add vec getting value examples

--- 8_collections/vecs/src/main.rs
+++ 8_collections/vecs/src/main.rs
@@ -6,4 +6,19 @@ fn main() {
     v1.push(6);
     v1.push(7);
     v1.push(8);
+
+    let v3 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v3[2];
+    println!("The third element is {}", third);
+
+    match v3.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let v4 = vec![1, 2, 3, 4, 5];
+
+    // let does_not_exist = &v4[100]; // panic
+    let does_not_exist = v4.get(100); // None
 }
