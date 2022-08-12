39444fa981bd4a0f3daea3f1dc2c6661c990baa4
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-12 12:58:27 +0900

    add String::from

--- 8_collections/strings/src/main.rs
+++ 8_collections/strings/src/main.rs
@@ -8,4 +8,7 @@ fn main() {
 
     // the method also works on a literal directly
     let s = "initial contets".to_string();
+
+    // ----------------------------------------
+    let s = String::from("initial contents");
 }
