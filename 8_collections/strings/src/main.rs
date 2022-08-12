ed4af1ccea26a00e457cb7489f9f0a14e750dcbb
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-12 12:57:00 +0900

    add: to_string example

--- 8_collections/strings/src/main.rs
+++ 8_collections/strings/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     // ----------------------------------------
     let mut s = String::new();
+
+    // ----------------------------------------
+    let data = "initial contents";
+    let s = data.to_string();
+
+    // the method also works on a literal directly
+    let s = "initial contets".to_string();
 }
