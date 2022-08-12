6fc812eb2388482e2d40b19bed4c5483b04cb904
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-12 15:00:47 +0900

    string push

--- 8_collections/strings/src/main.rs
+++ 8_collections/strings/src/main.rs
@@ -35,4 +35,8 @@ fn main() {
     let s2 = "bar";
     s1.push_str(s2);
     println!("s2 is {}", s2);
+
+    // ----------------------------------------
+    let mut s = String::from("lo");
+    s.push('l');
 }
