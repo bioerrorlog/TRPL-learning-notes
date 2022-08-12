ad66de384a218d39156c58563133b2fd50bc87af
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-12 15:02:44 +0900

    string + operator

--- 8_collections/strings/src/main.rs
+++ 8_collections/strings/src/main.rs
@@ -39,4 +39,9 @@ fn main() {
     // ----------------------------------------
     let mut s = String::from("lo");
     s.push('l');
+
+    // ----------------------------------------
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
 }
