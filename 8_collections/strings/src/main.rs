acf6e1dcdd06b4fede2818859a6a5fa7c0355f9b
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-12 15:14:30 +0900

    add format strings

--- 8_collections/strings/src/main.rs
+++ 8_collections/strings/src/main.rs
@@ -44,4 +44,18 @@ fn main() {
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
     let s3 = s1 + &s2;
+
+    // ----------------------------------------
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    // ----------------------------------------
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
 }
