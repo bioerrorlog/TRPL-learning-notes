10dcea2546beec7f113d1ddd1ca2161ab2cede8c
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-14 14:36:13 +0900

    add HashMap iterate

--- 8_collections/hashmaps/src/main.rs
+++ 8_collections/hashmaps/src/main.rs
@@ -11,4 +11,21 @@ fn main() {
     let initial_scores = vec![10, 50];
 
     let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    // ----------------------------------------
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value); // moved
+
+    // ----------------------------------------
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
