09b41cff1c254dc601b8f4e8a95a6830c69fb604
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-15 21:34:11 +0900

    multi fn in trait

--- 10_generics/traits/src/main.rs
+++ 10_generics/traits/src/main.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
 fn main() {
     pub trait Summary {
+        fn summarize_author(&self) -> String;
+
         fn summarize(&self) -> String {
-            String::from("(Read more...)")
+            format!("(Read more from {}...)", self.summarize_author())
         }
     }
 
@@ -18,7 +20,11 @@ fn main() {
     //         format!("{}, by {} ({})", self.headline, self.author, self.location)
     //     }
     // }
-    impl Summary for NewsArticle {}
+    impl Summary for NewsArticle {
+        fn summarize_author(&self) -> String {
+            format!("@{}", self.author)
+        }
+    }
 
     pub struct Tweet {
         pub username: String,
@@ -28,8 +34,8 @@ fn main() {
     }
 
     impl Summary for Tweet {
-        fn summarize(&self) -> String {
-            format!("{}: {}", self.username, self.content)
+        fn summarize_author(&self) -> String {
+            format!("@{}", self.username)
         }
     }
 
