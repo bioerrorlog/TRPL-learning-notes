7260aa6f0b6252d2fe96b1f50221ca292166f5e6
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-15 22:35:04 +0900

    return impl

--- 10_generics/traits/src/main.rs
+++ 10_generics/traits/src/main.rs
@@ -54,6 +54,17 @@ impl Summary for Tweet {
     }
 }
 
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
 fn main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
