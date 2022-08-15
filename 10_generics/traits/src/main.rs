62426bbf914d9fbe888e3da03c77ad27c46916fb
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-15 21:36:18 +0900

    del comment out

--- 10_generics/traits/src/main.rs
+++ 10_generics/traits/src/main.rs
@@ -15,11 +15,6 @@ fn main() {
         pub content: String,
     }
 
-    // impl Summary for NewsArticle {
-    //     fn summarize(&self) -> String {
-    //         format!("{}, by {} ({})", self.headline, self.author, self.location)
-    //     }
-    // }
     impl Summary for NewsArticle {
         fn summarize_author(&self) -> String {
             format!("@{}", self.author)
