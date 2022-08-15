ee7d5c67b8644adaa00661305b8dfaa75130ed4c
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-15 21:22:43 +0900

    example for default trait

--- 10_generics/traits/src/main.rs
+++ 10_generics/traits/src/main.rs
@@ -13,11 +13,12 @@ fn main() {
         pub content: String,
     }
 
-    impl Summary for NewsArticle {
-        fn summarize(&self) -> String {
-            format!("{}, by {} ({})", self.headline, self.author, self.location)
-        }
-    }
+    // impl Summary for NewsArticle {
+    //     fn summarize(&self) -> String {
+    //         format!("{}, by {} ({})", self.headline, self.author, self.location)
+    //     }
+    // }
+    impl Summary for NewsArticle {}
 
     pub struct Tweet {
         pub username: String,
@@ -34,12 +35,22 @@ fn main() {
 
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
+        content: String::from("of course, as you probably already know, people"),
         reply: false,
         retweet: false,
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
