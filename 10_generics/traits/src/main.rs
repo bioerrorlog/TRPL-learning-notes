7537c059b92faa6bd95a1033cec5bad4f2998cc5
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-15 21:46:49 +0900

    impl args

--- 10_generics/traits/src/main.rs
+++ 10_generics/traits/src/main.rs
@@ -1,39 +1,43 @@
 #![allow(unused)]
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
 fn main() {
-    pub trait Summary {
-        fn summarize_author(&self) -> String;
-
-        fn summarize(&self) -> String {
-            format!("(Read more from {}...)", self.summarize_author())
-        }
-    }
-
-    pub struct NewsArticle {
-        pub headline: String,
-        pub location: String,
-        pub author: String,
-        pub content: String,
-    }
-
-    impl Summary for NewsArticle {
-        fn summarize_author(&self) -> String {
-            format!("@{}", self.author)
-        }
-    }
-
-    pub struct Tweet {
-        pub username: String,
-        pub content: String,
-        pub reply: bool,
-        pub retweet: bool,
-    }
-
-    impl Summary for Tweet {
-        fn summarize_author(&self) -> String {
-            format!("@{}", self.username)
-        }
-    }
-
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
