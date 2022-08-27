8424109cdd915cafd7caed5544b8c842c72c6f66
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-27 16:27:56 +0900

    delete unnecessary function

--- 12_grep/minigrep/src/main.rs
+++ 12_grep/minigrep/src/main.rs
@@ -36,10 +36,3 @@ impl Config {
         Config { query, filename }
     }
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
